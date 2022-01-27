1bf2b054bbf53b53cf018d8c5a77bee3135ba043
ivansimplicio <ivansimplicio@outlook.com>
2022-01-27 17:51:13 -0300

    feat: creation of the service class for betting; validated the minimum amount allowed to place bets

--- app/Controllers/Http/BetsController.ts
+++ app/Controllers/Http/BetsController.ts
@@ -1,6 +1,6 @@
-import Game from 'App/Models/Game'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Bet from 'App/Models/Bet'
+import { betValidator, validateAllBets } from 'App/Services/BetService'
 import CreateBet from 'App/Validators/CreateBetValidator'
 import Updatebet from 'App/Validators/UpdateBetValidator'
 
@@ -13,36 +13,9 @@ export default class BetsController {
   public async store({ request, response }: HttpContextContract) {
     const userId = 2
     const payload = await request.validate(CreateBet)
-    const bets = await this.validateBets(userId, payload.games)
+    const bets = await validateAllBets(userId, payload.games)
     await Bet.createMany(bets)
     return response.created()
-  }
-
-  public async validateBets(userId: number, bets: any) {
-    const verifiedBets: Array<any> = []
-    for await (const bet of bets) {
-      const result = await this.validateBet({
-        userId,
-        gameId: bet.game_id,
-        chosenNumbers: bet.chosen_numbers,
-      })
-      verifiedBets.push(result)
-    }
-    return verifiedBets
-  }
-
-  public async validateBet(bet: any) {
-    const game = await Game.find(bet.gameId)
-    if (!game) return
-    const numbers = bet.chosenNumbers
-    if (!(game.maxNumber === numbers.length)) {
-      throw new Error('does not have the amount of numbers required by the game')
-    }
-    if (Math.min(...numbers) <= 0 || Math.max(...numbers) > game.range) {
-      throw new Error('the array has some value outside the range allowed by the game')
-    }
-    bet.chosenNumbers = numbers.toString()
-    return bet
   }
 
   public async show({ response, params }: HttpContextContract) {
@@ -60,11 +33,12 @@ export default class BetsController {
       return response.notFound()
     }
     const payload = await request.validate(Updatebet)
-    const resultValidation = await this.validateBet({
+    const resultValidation = await betValidator({
       gameId: bet.gameId,
       chosenNumbers: payload.chosen_numbers,
     })
-    bet.merge(resultValidation)
+    const { price, ...result } = resultValidation
+    bet.merge(result)
     await bet.save()
     return response.noContent()
   }
---
+++ app/Services/BetService.ts
@@ -0,0 +1,52 @@
+import Game from 'App/Models/Game'
+import Cart from 'App/MOdels/Cart'
+
+const validateAllBets = async (userId: number, bets: any) => {
+  const verifiedBets: Array<any> = []
+  let amount: number = 0
+  for await (const bet of bets) {
+    const validatedBet = await betValidator({
+      userId,
+      gameId: bet.game_id,
+      chosenNumbers: bet.chosen_numbers,
+    })
+    const { price, ...result } = validatedBet
+    amount += price
+    verifiedBets.push(result)
+  }
+  const minCartValue = await getMinCartValue(1)
+  if (minCartValue && minCartValue > amount) {
+    throw new Error(
+      `the value of your bets must total at least ${minCartValue}, but total only ${amount}`
+    )
+  }
+  return verifiedBets
+}
+
+const betValidator = async (bet: any) => {
+  const game = await Game.find(bet.gameId)
+  if (!game) return
+  const numbers = bet.chosenNumbers
+  if (!(game.maxNumber === numbers.length)) {
+    throw new Error('does not have the amount of numbers required by the game')
+  }
+  if (Math.min(...numbers) <= 0 || Math.max(...numbers) > game.range) {
+    throw new Error('the array has some value outside the range allowed by the game')
+  }
+  bet.chosenNumbers = sort(numbers).toString()
+  return { price: game.price, ...bet }
+}
+
+const sort = (numbers: Array<number>) => {
+  return numbers.sort((x, y) => x - y)
+}
+
+const getMinCartValue = async (cartId: number) => {
+  const cart = await Cart.find(cartId)
+  if (cart) {
+    return cart.minCartValue
+  }
+}
+
+export { betValidator }
+export { validateAllBets }
