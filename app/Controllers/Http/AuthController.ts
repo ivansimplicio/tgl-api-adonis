edd57527c98152dd3b2e2f8348735985fb7c59bb
ivansimplicio <ivansimplicio@outlook.com>
2022-02-01 15:12:59 -0300

    fix: properly validating user login, returning appropriate responses

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,19 +1,20 @@
 import User from 'App/Models/User'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import StandardError from 'App/Exceptions/Errors/StandardError'
+import LoginUser from 'App/Validators/LoginUserValidator'
 
 export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
-    const email = request.input('email')
-    const password = request.input('password')
+    const payload = await request.validate(LoginUser)
     try {
-      const user = await User.findBy('email', email)
-      const token = await auth.use('api').attempt(email, password, {
+      const user = await User.findBy('email', payload.email)
+      const token = await auth.use('api').attempt(payload.email, payload.password, {
         expiresIn: '7days',
         name: user?.serialize().email,
       })
       return { token, user: user?.serialize() }
     } catch {
-      return response.badRequest('Invalid credentials')
+      return response.badRequest(new StandardError('BAD_REQUEST', 400, 'Invalid credentials'))
     }
   }
 }
