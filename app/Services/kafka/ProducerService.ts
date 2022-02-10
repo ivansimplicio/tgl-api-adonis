3888b41ad01e38b4d7c8d5469d405f7a15ce140b
ivansimplicio <ivansimplicio@outlook.com>
2022-02-10 10:44:17 -0300

    fix: converting currency values to Brazilian format

--- app/Services/kafka/ProducerService.ts
+++ app/Services/kafka/ProducerService.ts
@@ -9,7 +9,11 @@ export default class ProducerService {
     this.sendToTopic('forgot_password', { url }, user, 'TGL: Recuperação de senha')
   }
 
-  public async produceTopicNewBetEmail(user: any, bets: any[], amount: number) {
+  public async produceTopicNewBetEmail(user: any, bets: any[], value: number) {
+    const amount = this.convertToReal(value)
+    bets.forEach((bet) => {
+      bet.price = this.convertToReal(bet.price)
+    })
     this.sendToTopic('new_bet', { bets, amount }, user, 'TGL: Nova aposta realizada!')
   }
 
@@ -17,12 +21,17 @@ export default class ProducerService {
     this.sendToTopic('call_players', {}, user, 'TGL: Sentimos sua falta...')
   }
 
-  public async produceTopicNotifyAdminEmail(admin: any, user: any, amount: number) {
+  public async produceTopicNotifyAdminEmail(admin: any, user: any, value: number) {
+    const amount = this.convertToReal(value)
     this.sendToTopic('notify_admin', { user, amount }, admin, 'TGL/Admin: Nova aposta realizada!')
   }
 
   private async sendToTopic(topic: string, content: {}, user: any, subject: string) {
     const message = { user, subject, content }
     await new Producer().send(topic.toString(), [{ value: JSON.stringify(message) }])
+  }
+
+  private convertToReal(value: number): string {
+    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
   }
 }
